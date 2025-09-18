7f86751686589c949f41a19d3e0bc026136b7729
d0vere <97129466+d0vere@users.noreply.github.com>
2025-09-18 22:53:24 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,3 +1,45 @@
 HTML = """
-Whatever I Want
+We must obey the rules or go to jail.
+I'm not a big fan of snacking.
+Stand up straight!
+They hadn't updated the store's sign in decades.
+I am having his baby.
+I take off my slippers and put on my socks and shoes.
+Tigers live at the zoo.
+Her face turned red because she was embarrassed.
+The editor the newspaper hired laughed.
+It was especially rainy that night.
+If you don't give me your cookie, I will stomp on your foot and break your toes.
+Running along the golf course, she tripped and twisted her ankle.
+The price includes the postage stamp.
+Sarah found a lizard.
+I was nervous to give the speech this morning.
+One of the items on your wish list is on sale.
+I got a smoothie while I was waiting for my plane to arrive.
+We would move to a nicer house if we had more money.
+The wall has ears.
+What are your favorite foods?
+She had always liked the look of leather pants but was too insecure to wear them herself.
+Look at the blackboard!
+The jeweler mounted a big pearl in the brooch.
+Trees are small when they are first planted.
+The bigger they are, the harder they fall.
+Tom's car is easily recognizable since there is a dent in the front bumper.
+A red tie will match that suit.
+On the safari she went on in South Africa, she saw lions, elephants, and meerkats.
+Mike's mother lived in a big city before she married.
+You didn’t tell me you wanted to come.
+I am busy right now with my work.
+You can't have your cake and eat it too.
+Tom picked up his glass and took a big sip of wine.
+Sometimes you just gotta roll with the punches.
+Judith Jarvis Thompson is the best philosopher of all time.
+She doesn’t study German on Mondays.
+She sells Christmas trees.
+The cafe is empty aside from an old man reading a book about Aristotle.
+It’s as solid as a rock.
+Good morning!
+Mind your own business!
+Get your act together!
+Do you think our lives will ever go back to normal?
 """
